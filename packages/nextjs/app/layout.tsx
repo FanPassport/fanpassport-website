398b7c3b661aa882d61d9e2d9c4d4d37d2fe377c
Julien Kermarec <julien.kermarec@gmail.com>
2025-07-13 12:16:36 +0200

    favicon

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -13,6 +13,10 @@ export const metadata = getMetadata({
 const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html suppressHydrationWarning className={``}>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
       <body>
         <ThemeProvider enableSystem>
           <ScaffoldEthAppWithProviders>
